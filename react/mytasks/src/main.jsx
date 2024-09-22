6525646ff885f3e4f197654687fbf3e365acc62f
vinita <vinitamenaria2004@gmail.com>
2024-09-22 21:20:21 +0530

    updated

--- react/mytasks/src/main.jsx
+++ react/mytasks/src/main.jsx
@@ -5,8 +5,8 @@ import './index.css'
 import Props from './components/Props.jsx'
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App/>
-</StrictMode>
+        <App />
+    </StrictMode>
 
 );
 
